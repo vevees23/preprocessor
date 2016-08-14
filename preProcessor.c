b337dc669899f71a9ae7555ee6b1607199e1c35a
timon-pumba <tp@zaktitas.com>
2016-08-14 09:21:50 +0530

    Intial preProcessor code #thsi removes only comments
    preprcoessor in c removes the comments , expand heaader files , reslove
    macros , ...etc
    Initial this code only for remove comments in souce file and saves file
    with *.i extention

---
+++ preProcessor.c
@@ -0,0 +1,69 @@
+#include<stdio.h>
+#include<string.h>
+main(int argc ,char **argv)
+{
+    FILE *fp,*fp1;
+    char d[20],buff[200],buff1[200],*in,*inf;
+    int i,pos,flag;
+    for(i=0;i<strlen(argv[1]) && argv[1][i]!='.';i++)
+    {
+        d[i]=argv[1][i];
+    }
+    d[i++]='.';
+    d[i++]='i';
+    d[i]='\0';
+    //fgjjkj
+    fp=fopen(argv[1],"r");
+    if(fp==NULL)
+    {
+        printf("no file found\n");
+        return;
+    }
+    fp1=fopen(d,"w");
+    while(fgets(buff,200,fp))
+    {
+        //l=strlen(buff);
+        in=buff;
+        if(inf=strstr(buff,"//"))
+        {
+            pos=inf-in;
+          
+            buff[pos]='\n';
+            buff[pos+1]='\0';
+        }
+        in=buff;
+        if(inf=strstr(buff,"/*"))
+        {
+            pos=inf-in;
+              
+            if(strstr(buff,"*/"));
+            else
+            while(1)
+            {   
+               if(fgets(buff1,200,fp))
+               {
+              if(strstr(buff1,"*/"))
+              {flag=1;
+              break;
+              }
+            }
+            else
+
+            {
+                flag=0;
+                break;
+            }
+            }
+            buff[pos]='\n';
+            buff[pos+1]='\0';
+        }
+        fputs(buff,fp1);
+        if(flag==0)
+        {
+            printf("end comment not found\n");
+            return;
+        }
+    }
+fclose(fp1);
+fclose(fp);
+}
