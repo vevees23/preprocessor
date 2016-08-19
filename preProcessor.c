7ee05697f5b594c898e575058b75411c6fdc99d2
timon-pumba <tp@zaktitas.com>
2016-08-19 08:42:57 +0530

    Second step code to identify and expand local header and Predefine header files
    Lots of complex condition checks used #optimization required;
    If header file contain another header file it will not expand ,have to
    write code for it ;
    Code and memory optimization required:

--- preProcessor.c
+++ preProcessor.c
@@ -1,12 +1,22 @@
 #include<stdio.h>
 #include<string.h>
-void expandHeader(FILE *fp1,char *p)
+void expandHeader(FILE *fp1,char *p,int flag)
 {
     char dir[4][50]={"/usr/include/"},buf1[300];
     FILE *fph;
     strcat(dir[0],p);
+    if(flag==2)
+    {
+        fph=fopen(p,"r");
+    
+    if (fph==NULL)
+    {
     fph=fopen(dir[0],"r");
-    while(fgets(buf1,200,fph))
+    }
+    }
+    else if(flag==1)
+    fph=fopen(dir[0],"r");
+    while(fgets(buf1,300,fph))
     {
         fputs(buf1,fp1);
     }
@@ -15,11 +25,72 @@ void remCmnt(FILE *fp,FILE *fp1)
 {
     char buff[200],buff1[200],*in,*inf;
     int i,pos;
-    expandHeader(fp1,"stdio.h");
+    //expandHeader(fp1,"stdio.h");
     while(fgets(buff,200,fp))
     {
         //l=strlen(buff);
         in=buff;
+        if(strstr(buff,"#include"))
+        {
+            char *pos,*intial;
+            char he[20];
+            int flag,index1,index2,i;
+            intial=buff;
+           // printf("inloop");
+            if(pos=strchr(buff,'<'))
+            {   
+                //printf("inloop1");
+                index1=pos-intial;
+                if(pos=strchr(buff,'>'))
+                {
+                   // printf("inloop2\n");
+                    index2=pos-intial;
+                    flag=1;
+                    //printf("%d,%d",index1,index2);
+                    if((index1==(index2-1)))
+                    continue;
+                    for(i=0,index1++;index1<index2;index1++,i++)
+                   {
+                        he[i]=buff[index1];
+                    }
+                    he[i]=0;
+                   // printf("%s\n",he);
+                    expandHeader(fp1,he,flag);
+                }
+                 else{
+                        printf("\nend tag not found for hearfile\n ");
+                        return;
+                }
+                
+            }
+              if(pos=strchr(buff,'"'))
+            {   
+              
+                index1=pos-intial;
+                if(pos=strrchr(buff,'"'))
+                {
+                  
+                    index2=pos-intial;
+                    flag=2;
+                    //printf("%d,%d",index1,index2);
+                    if((index1==(index2-1)))
+                    continue;
+                    for(i=0,index1++;index1<index2;index1++,i++)
+                   {
+                        he[i]=buff[index1];
+                    }
+                    he[i]=0;
+                   // printf("%s\n",he);
+                    expandHeader(fp1,he,flag);
+                }
+                 else{
+                        printf("\nend tag not found for hearfile \n");
+                        return;
+                }
+                
+            }
+            continue;
+        }
         if(inf=strstr(buff,"//"))
         {
             pos=inf-in;
